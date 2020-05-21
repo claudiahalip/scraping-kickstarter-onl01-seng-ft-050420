26ca6b53971c29a7f563ae9c5da309afb0bff3ee
claudia v <claudia4ro@yahoo.com>
2020-05-21 03:40:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,9 @@
- require Nokogiri
+ require 'nokogiri'
 
 
 def create_project_hash
   # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  
 end
